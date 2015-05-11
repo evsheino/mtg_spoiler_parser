5b1225ac2b7e3315c2a2b2ebcef560d14df829c6
Erkki Heino <evsheino@gmail.com>
2015-05-11 13:14:48 +0300

    Update parser for new MtGSalvation spoiler layout

--- spoiler_parser.py
+++ spoiler_parser.py
@@ -6,50 +6,57 @@ import codecs
 import re
 import sys
 
+# Mana formats
+NORMAL_MANA = "{}"
+PHYREXIAN_MANA = "{{!{}}}"
+HYBRID_MANA = "({}/{})"
+
+# Spoiler mana formats for regex matching
+NORMAL_MANA_RE = r"([wWuUbBrRgG])"
+SPOILER_PHYREXIAN_MANA_RE = r"\(({})/[pP]\)".format(NORMAL_MANA_RE)
+SPOILER_HYBRID_MANA_RE = r"\(({0})/({0})\)".format(NORMAL_MANA_RE)
+
+# Mana cost transform dict
+MANA_DICT = { NORMAL_MANA_RE: NORMAL_MANA, 
+        SPOILER_PHYREXIAN_MANA_RE: PHYREXIAN_MANA,
+        SPOILER_HYBRID_MANA_RE: HYBRID_MANA }
+
 class MtgCard(object):
     """
     An MtG card.
     """
 
-    def __init__(self, a, mtg_set):
+    def __init__(self, elem, mtg_set):
         """
-        @param a: the a element in the spoiler source as a BeautifulSoup element.
+        @param elem: an element containing the information of the card 
+                     in the spoiler source as a BeautifulSoup element.
         @param mtg_set: the set of the card.
         """
 
         self.mtg_set = mtg_set
-        self.name = parse_name(a).replace(u'’', u'').replace(u',', u'').replace(u"'", '')
-        self.rarity = parse_rarity(a)
-        self.card_type = parse_type(a)
-        mana_cost = parse_mana_cost(a)
+        self.name = parse_name(elem).replace(u'’', u'').replace(u',', u'').replace(u"'", '')
+        self.rarity = parse_rarity(elem)
+        self.card_type = parse_type(elem)
+        mana_cost = parse_mana_cost(elem)
         self.mana_cost = u''.join(mana_cost)
         self.converted_mana_cost = parse_converted_cost(mana_cost)
         self.color = parse_color(mana_cost)
-        self.text = parse_text(a)
-
-        power_and_toughness = parse_power_and_toughness(a)
+        self.text = parse_text(elem)
         try:
-            self.power = power_and_toughness[0]
-            self.toughness = power_and_toughness[1]
-        except IndexError:
+            self.power, self.toughness = parse_power_and_toughness(a)
+        except TypeError:
             # No power/toughness for this card
             self.power = u''
             self.toughness = u''
 
-def parse_name(a):
-    name = a.attrs.get('name')
-    if name:
-        name = unicode(name)
-        if not name.isnumeric():
-            return name.strip()
-    raise TypeError
+def parse_name(elem):
+    return parse_elem_text(elem.find("h2"))
 
-def parse_rarity(a):
-    return a.parent.parent.next_sibling.next_sibling.find('img').attrs.get('alt') \
-        .strip()[:1]
+def parse_rarity(elem):
+    return elem.find("header")["class"][1][0].upper()
 
-def parse_type(a):
-    return a.parent.parent.next_sibling.next_sibling.td.string.strip()
+def parse_type(elem):
+    return elem.find("span", "t-spoiler-type").text
 
 def parse_symbol(symbol):
     """
@@ -57,31 +64,31 @@ def parse_symbol(symbol):
     """
 
     s = unicode(symbol)
-    if not s.isnumeric():
-        if len(s) == 2:
-            # Hybrid mana
-            s = u'({}/{})'.format(s[0], s[1]).upper()
-    return s
+    if s.isnumeric():
+        return s
 
-def parse_mana_cost(a):
+    for regex, mana_format in MANA_DICT.items():
+        r = re.match(regex, s)
+        if r:
+            mana_symbols = r.groups()
+            if len(mana_symbols) > 1:
+                return unicode(mana_format.format(*mana_symbols[1:]))
+            return unicode(mana_format.format(mana_symbols[0]))
+
+    return u''
+
+def parse_mana_cost(elem):
     """
-    Get the mana cost of a card.
+    Get the mana cost of a card as a list.
 
-    @param a: the a element of the card in the MtgSalvation spoiler.
+    @param elem: the element of the card in the MtgSalvation spoiler.
     """
-
     mana_cost = []
-    td = a.parent.next_sibling.next_sibling
-    
-    for e in td:
-        try:
-            symbol = parse_symbol(e.attrs.get('alt').upper())
-        except AttributeError:
-            if e != u'//':
-                continue
-            symbol = e
-        mana_cost.append(symbol)
+    spoiler_mc = parse_elem_text(elem.find("ul", "t-spoiler-mana"), as_list=True)
+    for symbol in spoiler_mc:
+        mana_cost.append(parse_symbol(symbol))
     return mana_cost
+
 
 def parse_color(mana_cost):
     color = u''
@@ -98,7 +105,7 @@ def strip_symbol(symbol):
     return m
 
 def parse_converted_cost(mana_cost):
-    skip = ['X', '//']
+    skip = ['X', '//', '!']
     cost = 0
     for symbol in mana_cost:
         symbol = unicode(symbol)
@@ -124,52 +131,45 @@ def parse_toughness(a):
         toughness = u''
     return toughness
 
-def parse_text(a):
-    tag = a.parent.parent.parent.find(colspan='2')
-    return parse_elem_text(tag).strip()
+def parse_text(elem):
+    text = elem.find("input")["value"]
+    return u' '.join(re.sub(r"\[[/]*mana\]", "", text).split())
 
-def parse_elem_text(tag):
+def parse_elem_text(tag, as_list=False):
     """
     Parse the given tag recursively and return the contents stripped of all
-    tags. Include image tags' alt text.
+    tags.
     """
-    if tag.name == 'img':
-        return tag.attrs.get('alt')
-    text = u''
+    if as_list:
+        text = []
+    else:
+        text = u''
     # Try to parse the tag recursively. If it's a NavigableString, return it and end the recursion.
     try:
         for content in tag.contents:
-            text += parse_elem_text(content)
-        return text
+            text += parse_elem_text(content, as_list)
+        return text if as_list else text.strip()
     except AttributeError:
+        if tag.strip() == "":
+            return [] if as_list else u''
         # NavigableString encountered: return it
+        if as_list:
+            return [tag.strip()]
         return u'{} '.format(tag.strip())
 
 
-def parse_power_and_toughness(a):
+def parse_power_and_toughness(elem):
     """
     Parse the power and toughness.
 
     Return the power and toughness as a list: [power, toughness]
     """
 
-    pt = []
-    parent = a.parent.parent
-    for elem in parent.next_siblings:
-        try:
-            for child in elem.children:
-                try:
-                    pt_string = child.string.strip()
-                except AttributeError:
-                    continue
-                try:
-                    int(pt_string.replace(u'/', u'').replace(u'*', u'0'))
-                    pt = child.string.strip().split('/')
-                except ValueError:
-                    continue
-        except AttributeError:
-            pass
-    return pt 
+    try:
+        return elem.find("span", "t-spoiler-stat").text.strip().split('/')
+    except AttributeError:
+        # No power/toughness
+        return None
 
 def write_card_list(cards, output_file):
     """
--- spoiler_parser_tests.py
+++ spoiler_parser_tests.py
@@ -6,130 +6,153 @@ import unittest
 class TestParser(unittest.TestCase):
 
     def setUp(self):
-        assault_griffin_table = """
-            <table border="0" cellspacing="0" cellpadding="5" width="290" align="center" style="border: 1px solid black;background-color:white;color: black;">
-                <tr style="background-color: #ffffcc;border-bottom: 1px solid #aaa;">
-                    <td width="220px">
-                        <a name="6787"></a>
-                        <a name="Assault Griffin"></a>
-                        <h3 style="margin: 0;">*Assault Griffin</h3>
-                    </td>
-                    <td align="right" width="80px">
-                        <img src="http://forums.mtgsalvation.com/images/smilies/mana3.gif" alt="3" class="inlineimg" style="border: none;" />
-                        <img src="http://forums.mtgsalvation.com/images/smilies/manaw.gif" alt="w" class="inlineimg" style="border: none;" />
-                    </td>
-                </tr>
-                <tr style="border-bottom: 1px dotted #aaa;">
-                    <td width="220px">
-                        Creature - Griffin
-                    </td>
-                    <td align="right" width="80px">
-                        <img src="http://mtgsalvation.com/images/spoiler/gatecrash-common.gif" alt="Common" />
-                    </td>
-                </tr>
-                <tr>
-                    <td colspan="2">
-                        Flying
-                    </td>
-                </tr><tr><td colspan="3"><i>"The Simic offer a prize to any biomancer who can breed a krasis to match a griffin in the air. It's never been claimed."<br />
-            Libuse, Boros sergeant</i></td></tr>
-                <tr style="border-top: 1px dotted #aaa;">
-                    <td>
-                        Illus. Eric Velhagen <i>#4/249</i>
-                    </td>
-                    <td align="right">
-                        3/2
-                    </td>
-                </tr>
-            </table>
+        mighty_leap_elem = """
+<div class="spoiler-card-text">
+                                       
+<div class="t-spoiler card-color-White card-type-Instant"  id="Mighty Leap">
+    <div class="t-spoiler-container">
+        <header class="t-spoiler-header common">
+            
+                <h2><a class="j-search-html" href="http://www.mtgsalvation.com/cards/modern-masters-2015-edition/25138-mighty-leap">Mighty Leap</a></h2>
+            
+            <ul class="t-spoiler-mana">
+                
+                    <span class="mana-icon mana-colorless-01 tip" title="1 Colorless Mana">1</span>
+                
+                    <span class="mana-icon mana-white tip" title="1 White Mana">W</span>
+                
+            </ul>
+        </header>
+        <section class="t-spoiler-content">
+            <div class="t-spoiler-meta">
+                <span class="t-spoiler-type j-search-html">Instant</span>
+                <span class="t-spoiler-rarity"><span class="mtg-set-icon mtg-set-modern-masters-2015-edition-common" style="background: url('http://media-dominaria.cursecdn.com/avatars/thumbnails/66/14/22/14/MM2.png') width: 22px; height: 14px;"></span></span>
+            </div>
+            
+            <div class="t-spoiler-ability">
+                
+                <p>Target creature gets +2/+2 and gains flying until end of turn.</p>
+                
+                <input class="j-search-val" type="hidden" value="Target creature gets +2/+2 and gains flying until end of turn." />
+            </div>
+            
+            <div class="t-spoiler-flavor">
+                <p>&quot;The southern fortress taken by invaders? Heh, sure.... when elephants fly.&quot;
+-Brezard Skeinbow, captain of the guard.</p>
+            </div>
+            
+            <div class="t-spoiler-edition">
+                <span class="t-spoiler-artist"><p>illus. RK Post # 24/249</p></span>
+            </div>
+        </section>
+        <footer class="t-spoiler-footer">
+            <p>
+                
+                    <span class="tip" title="Mothership">SRC</span>
+                
+                    <a href="http://www.mtgsalvation.com/cards?filter-search=Mighty Leap" class="tip" title="This card is a reprint">RP</a>
+                
+            </p>
+            
+        </footer>
+    </div>
+</div>
+
+</div>
         """
 
-        simic_fluxmage_table = """
-            <table border="0" cellspacing="0" cellpadding="5" width="290" align="center" style="border: 1px solid black;background-color:white;color: black;">
-		    <tr style="background-color: #99ccff;border-bottom: 1px solid #aaa;">
-		        <td width="220px">
-		            <a name="6652"></a>
-		            <a name="Simic Fluxmage"></a>
-		            <h3 style="margin: 0;">
-                        <a href="http://forums.mtgsalvation.com/attachment.php?attachmentid=138020&d=1356148776" style="color: #3E5E7B;">Simic Fluxmage <img src="http://mtgsalvation.com/images/image.png" />
-                        </a>
-                    </h3>
-		        </td>
-		        <td align="right" width="80px">
-                    <img src="http://forums.mtgsalvation.com/images/smilies/mana2.gif" alt="2" class="inlineimg" style="border: none;" />
-                    <img src="http://forums.mtgsalvation.com/images/smilies/manau.gif" alt="u" class="inlineimg" style="border: none;" />
-                </td>
-		    </tr>
-		    <tr style="border-bottom: 1px dotted #aaa;">
-		        <td width="220px">Creature - Merfolk Wizard</td>
-		        <td align="right" width="80px"><img src="http://mtgsalvation.com/images/spoiler/gatecrash-uncommon.gif" alt="Uncommon" /></td>
-		    </tr>
-		    <tr>
-		        <td colspan="2">
-                    Evolve <i><i>(Whenever a creature enters the battlefield under your control, if that creature has greater power or toughness than this creature, put a +1/+1 counter on this creature.)</i></i><br />
-                    <img src="http://forums.mtgsalvation.com/images/smilies/mana1.gif" alt="1" /><img src="http://forums.mtgsalvation.com/images/smilies/manau.gif" alt="{U}" />
-                    , <img src="http://forums.mtgsalvation.com/images/smilies/tap.gif" alt="{T}" />: Move a +1/+1 counter from Simic Fluxmage onto target creature.
-                </td>
-            </tr>
-            <tr style="border-top: 1px dotted #aaa;">
-                <td>Illus. Karl Kopinski <i>#49/249</i></td>
-                <td align="right">1/2</td>
-            </tr>
-        </table>
+        water_servant_elem = """
+<div class="spoiler-card-text">
+                                       
+<div class="t-spoiler card-color-Blue card-type-Creature"  id="Water Servant">
+    <div class="t-spoiler-container">
+        <header class="t-spoiler-header uncommon">
+            
+                <h2><a class="j-search-html" href="http://www.mtgsalvation.com/cards/modern-masters-2015-edition/25238-water-servant">Water Servant</a></h2>
+            
+            <ul class="t-spoiler-mana">
+                
+                    <span class="mana-icon mana-colorless-02 tip" title="2 Colorless Mana">2</span>
+                
+                    <span class="mana-icon mana-blue tip" title="1 Blue Mana">U</span>
+                
+                    <span class="mana-icon mana-blue tip" title="1 Blue Mana">U</span>
+                
+            </ul>
+        </header>
+        <section class="t-spoiler-content">
+            <div class="t-spoiler-meta">
+                <span class="t-spoiler-type j-search-html">Creature - Elemental</span>
+                <span class="t-spoiler-rarity"><span class="mtg-set-icon mtg-set-modern-masters-2015-edition-uncommon" style="background: url('http://media-dominaria.cursecdn.com/avatars/thumbnails/66/15/22/14/MM2.png') width: 22px; height: 14px;"></span></span>
+            </div>
+            
+            <div class="t-spoiler-ability">
+                
+                <p><span class="tip mana-icon mana-blue" title="1 Blue Mana">U</span>: Water Servant gets +1/-1 until end of turn.
+</p>
+                
+                <p></p>
+                
+                <p><span class="tip mana-icon mana-blue" title="1 Blue Mana">U</span>: Water Servant gets -1/+1 until end of turn.</p>
+                
+                <input class="j-search-val" type="hidden" value="{U}: Water Servant gets +1/-1 until end of turn.
+
+{U}: Water Servant gets -1/+1 until end of turn." />
+            </div>
+            
+            <div class="t-spoiler-flavor">
+                <p>&quot;This creature has innate perceptiveness. It knows when to rise and when to vanish into the tides.&quot;—Jestus Dreya, Of Elements and Eternity</p>
+            </div>
+            
+            <div class="t-spoiler-edition">
+                <span class="t-spoiler-artist"><p>illus. Igor Kieryluk # 69/249</p></span>
+            </div>
+        </section>
+        <footer class="t-spoiler-footer">
+            <p>
+                
+                    <a href="http://www.mtgsalvation.com/cards?filter-search=Water Servant" class="tip" title="This card is a reprint">RP</a>
+                
+            </p>
+            
+                    <span class="t-spoiler-stat">3/4</span>
+                
+        </footer>
+    </div>
+</div>
+
+                                </div>
         """
 
-        armed_dangerous_table = """
-            <table border="0" cellspacing="0" cellpadding="5" width="290" align="center" style="border: 1px solid black;background-color:white;color: black;">
-                        <tr style="background: url(http://mtgsalvation.com/images/spoiler/bg/.gif) 50% 50%;border-bottom: 1px solid #aaa;">
-                            <td width="220px">
-                                <a name="6946"></a>
-                                <a name="Armed // Dangerous"></a>
-                                <h3 style="margin: 0;"><a href="http://forums.mtgsalvation.com/attachment.php?attachmentid=141190&d=1365601141" style="color: #3E5E7B;">Armed // Dangerous <img src="http://mtgsalvation.com/images/image.png" /></a></h3>
-                            </td>
-                            <td align="right" width="80px"><img src="http://forums.mtgsalvation.com/images/smilies/mana1.gif" alt="1" class="inlineimg" style="border: none;" /><img src="http://forums.mtgsalvation.com/images/smilies/manar.gif" alt="r" class="inlineimg" style="border: none;" />//<img src="http://forums.mtgsalvation.com/images/smilies/mana3.gif" alt="3" class="inlineimg" style="border: none;" /><img src="http://forums.mtgsalvation.com/images/smilies/manag.gif" alt="g" class="inlineimg" style="border: none;" /></td>
-                        </tr>
-                        <tr style="border-bottom: 1px dotted #aaa;">
-                            <td width="220px">Sorcery // Sorcery</td>
-                            <td align="right" width="80px"><img src="http://mtgsalvation.com/images/spoiler/dragons-maze-uncommon.gif" alt="Uncommon" /></td>
-                        </tr>
-                        <tr>
-                            <td colspan="2">Target creature gets +1/+1 and gains double strike until end of turn.<br />
-            //<br />
-            All creatures able to block target creature do so this turn.<br />
-            Fuse <i>(You may cast one or both halves of this card from your hand.)</i></td></tr><tr style="border-top: 1px dotted #aaa;"><td>Illus. David Palumbo <i>#122/166</i></td><td align="right"></td></tr></table>
-        """
+        self.mighty_leap = BeautifulSoup(mighty_leap_elem)
 
-        soup = BeautifulSoup(assault_griffin_table)
-        self.a = soup.table.find_all('a')[1]
-
-        soup = BeautifulSoup(simic_fluxmage_table)
-        self.fluxmage_a = soup.table.find_all('a')[1]
-
-        soup = BeautifulSoup(armed_dangerous_table)
-        self.armed_dangerous_a = soup.table.find_all('a')[1]
+        self.water_servant = BeautifulSoup(water_servant_elem)
 
     def test_parse_name(self):
-        name = sp.parse_name(self.a)
-        self.assertEqual(name, u"Assault Griffin")
+        name = sp.parse_name(self.mighty_leap)
+        self.assertEqual(name, u"Mighty Leap")
 
     def test_parse_rarity(self):
-        rarity = sp.parse_rarity(self.a)
+        rarity = sp.parse_rarity(self.mighty_leap)
         self.assertEqual(rarity, u"C")
 
     def test_parse_type(self):
-        card_type = sp.parse_type(self.a)
-        self.assertEqual(card_type, u"Creature - Griffin")
+        card_type = sp.parse_type(self.mighty_leap)
+        self.assertEqual(card_type, u"Instant")
 
     def test_parse_mana_cost(self):
-        mana_cost = sp.parse_mana_cost(self.a)
-        self.assertEqual(mana_cost, [u"3", u"W"])
+        mana_cost = sp.parse_mana_cost(self.mighty_leap)
+        self.assertEqual(mana_cost, [u"1", u"W"])
 
     def test_parse_symbol(self):
         symbol = sp.parse_symbol(u"W")
         self.assertEqual(symbol, u"W")
 
-        symbol = sp.parse_symbol(u"rw")
+        symbol = sp.parse_symbol(u"(R/W)")
         self.assertEqual(symbol, u"(R/W)")
+
+        symbol = sp.parse_symbol(u"(R/P)")
+        self.assertEqual(symbol, u"{!R}")
 
     def test_parse_color(self):
         color = sp.parse_color([u"3", u"W"])
@@ -148,48 +171,22 @@ class TestParser(unittest.TestCase):
         colors = sp.strip_symbol(u"X")
         self.assertEqual(colors, [])
 
-    def test_parse_mana_cost(self):
-        mana_cost = sp.parse_mana_cost(self.a)
-        self.assertEqual(mana_cost, [u"3", u"W"])
-
     def test_parse_converted_cost(self):
         cost = sp.parse_converted_cost([u"X", u"X", u"(R/W)"])
         self.assertEqual(cost, 1)
 
     def test_parse_power_and_toughness(self):
-        pt = sp.parse_power_and_toughness(self.a)
-        self.assertEqual(pt, [u"3", u"2"])
+        pt = sp.parse_power_and_toughness(self.water_servant)
+        self.assertEqual(pt, [u"3", u"4"])
 
     def test_parse_text(self):
         self.maxDiff = None
-        text = sp.parse_text(self.a)
-        self.assertEqual(text, u"Flying")
+        text = sp.parse_text(self.mighty_leap)
+        self.assertEqual(text, u"Target creature gets +2/+2 and gains flying until end of turn.")
 
-        text = sp.parse_text(self.fluxmage_a)
-        real_text = u"Evolve (Whenever a creature enters the battlefield under your control, if that creature has greater power or toughness than this creature, put a +1/+1 counter on this creature.)  1{U}, {T}: Move a +1/+1 counter from Simic Fluxmage onto target creature."
+        text = sp.parse_text(self.water_servant)
+        real_text = u"{U}: Water Servant gets +1/-1 until end of turn. {U}: Water Servant gets -1/+1 until end of turn."
         self.assertEqual(text, real_text)
-
-    def _test_new_parse_text(self):
-        td = """
-            <td colspan="2">
-                Evolve <i><i>(Whenever a creature enters the battlefield under your control, if that creature has greater power or toughness than this creature, put a +1/+1 counter on this creature.)</i></i><br />
-                <img src="http://forums.mtgsalvation.com/images/smilies/mana1.gif" alt="1" />
-                <img src="http://forums.mtgsalvation.com/images/smilies/manau.gif" alt="{U}" />
-                , <img src="http://forums.mtgsalvation.com/images/smilies/tap.gif" alt="{T}" />: Move a +1/+1 counter from Simic Fluxmage onto target creature.
-            </td>
-        """
-        td = """
-		        <td colspan="2">Flying<br />
-Extort <i>(Whenever you cast a spell, you pay <img
-src="http://forums.mtgsalvation.com/images/smilies/manaWB.gif" alt="{WB}" />. If
-you do, each opponent loses 1 life and you gain that much life.)</i></td>
-"""
-        soup = BeautifulSoup(td)
-        text = parse_text(soup.td)
-
-    def test_parse_split_card_mana_cost(self):
-        mana_cost = sp.parse_mana_cost(self.armed_dangerous_a)
-        self.assertEqual(mana_cost, [u'1', u'R', u'//', u'3', u'G'])
 
 if __name__ == "__main__":
     unittest.main()
